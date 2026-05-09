fd14c22077b76dee1fc066ab435d2c0f7af84271
Timothy Maersk Borja <timothymaerskborja@tim-3.local>
2026-05-09 14:17:18 +0800

    alignment

--- packages/dependencies/lib/src/models/clinic_type.dart
+++ packages/dependencies/lib/src/models/clinic_type.dart
@@ -6,57 +6,57 @@ enum ClinicType {
   pedia;
 
   String get displayName => switch (this) {
-        ClinicType.dental => 'Dental',
-        ClinicType.optometry => 'Eye Care',
-        ClinicType.pedia => 'Pediatrics',
-      };
+    ClinicType.dental => 'Dental',
+    ClinicType.optometry => 'Eye Care',
+    ClinicType.pedia => 'Pediatrics',
+  };
 
   String get clinicName => switch (this) {
-        ClinicType.dental => 'Smile Dental Clinic',
-        ClinicType.optometry => 'ClearVision Eye Clinic',
-        ClinicType.pedia => 'Little Stars Pediatrics',
-      };
+    ClinicType.dental => 'D-Touch Dental Clinic',
+    ClinicType.optometry => 'ClearVision Eye Clinic',
+    ClinicType.pedia => 'Little Stars Pediatrics',
+  };
 
   Color get seedColor => switch (this) {
-        ClinicType.dental => const Color(0xFF00897B),
-        ClinicType.optometry => const Color(0xFF1565C0),
-        ClinicType.pedia => const Color(0xFFE65100),
-      };
+    ClinicType.dental => const Color(0xFF00897B),
+    ClinicType.optometry => const Color(0xFF1565C0),
+    ClinicType.pedia => const Color(0xFFE65100),
+  };
 
   IconData get icon => switch (this) {
-        ClinicType.dental => Icons.medical_services_outlined,
-        ClinicType.optometry => Icons.remove_red_eye_outlined,
-        ClinicType.pedia => Icons.child_care_outlined,
-      };
+    ClinicType.dental => Icons.medical_services_outlined,
+    ClinicType.optometry => Icons.remove_red_eye_outlined,
+    ClinicType.pedia => Icons.child_care_outlined,
+  };
 
   List<String> get services => switch (this) {
-        ClinicType.dental => [
-            'Teeth Cleaning',
-            'Tooth Extraction',
-            'Dental Filling',
-            'Root Canal',
-            'Braces Adjustment',
-            'Teeth Whitening',
-            'Dental X-Ray',
-            'Wisdom Tooth Removal',
-          ],
-        ClinicType.optometry => [
-            'Eye Examination',
-            'Contact Lens Fitting',
-            'Eyeglasses Prescription',
-            'Glaucoma Screening',
-            'LASIK Consultation',
-            'Retinal Exam',
-            'Low Vision Assessment',
-          ],
-        ClinicType.pedia => [
-            'Well-Child Visit',
-            'Vaccination',
-            'Sick Visit',
-            'Growth Assessment',
-            'Development Screening',
-            'Newborn Check-up',
-            'School Physical',
-          ],
-      };
+    ClinicType.dental => [
+      'Teeth Cleaning',
+      'Tooth Extraction',
+      'Dental Filling',
+      'Root Canal',
+      'Braces Adjustment',
+      'Teeth Whitening',
+      'Dental X-Ray',
+      'Wisdom Tooth Removal',
+    ],
+    ClinicType.optometry => [
+      'Eye Examination',
+      'Contact Lens Fitting',
+      'Eyeglasses Prescription',
+      'Glaucoma Screening',
+      'LASIK Consultation',
+      'Retinal Exam',
+      'Low Vision Assessment',
+    ],
+    ClinicType.pedia => [
+      'Well-Child Visit',
+      'Vaccination',
+      'Sick Visit',
+      'Growth Assessment',
+      'Development Screening',
+      'Newborn Check-up',
+      'School Physical',
+    ],
+  };
 }
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Build specific app:
+
+Patient:
+
+flutter build web --project-dir apps/patient_app
+
+Doctor:
+
+flutter build web --project-dir apps/doctor_app
+
+Assistant:
+
+flutter build web --project-dir apps/assistant_app
+
+Admin web:
+
+flutter build web --project-dir apps/admin_app
+
+
